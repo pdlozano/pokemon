d68aa3261be50bec775e3f544f9ed2e7bd4eb0ab
David Lozano <hello@davidlozano.me>
2021-10-31 12:54:58 +0800

    Add Pascal Casing to Strings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+declare global {
+    // to access the global type String
+    interface String {
+        toPascalCase(): string;
+    }
 }
 
-export default MyApp
+String.prototype.toPascalCase = function (): string {
+    return this.replace(new RegExp(/[-_]+/, "g"), " ")
+        .replace(new RegExp(/[^\w\s]/, "g"), "")
+        .replace(
+            new RegExp(/\s+(.)(\w*)/, "g"),
+            (_, _2, _3) => `${_2.toUpperCase() + _3.toLowerCase()}`
+        )
+        .replace(new RegExp(/\w/), (s) => s.toUpperCase());
+};
+
+function MyApp({ Component, pageProps }: AppProps) {
+    return <Component {...pageProps} />;
+}
+
+export default MyApp;
