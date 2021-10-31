080d7751cf904aa718d4db33a6b4824dbcf3cf4f
David Lozano <hello@davidlozano.me>
2021-10-31 12:52:25 +0800

    Add Pokemon Types
    The values stored in the enum are the hex values
    of the color for each type

---
+++ modules/PokemonTypes.tsx
@@ -0,0 +1,26 @@
+enum PokemonTypes {
+    /*
+     * The values stored here are the hex values of the
+     * color for the type
+     */
+    Normal = "#aa9",
+    Fire = "#f42",
+    Water = "#39f",
+    Electric = "#fc3",
+    Grass = "#7c5",
+    Ice = "#6cf",
+    Fighting = "#b54",
+    Poison = "#a59",
+    Ground = "#db5",
+    Flying = "#89f",
+    Psychic = "#f59",
+    Bug = "#ab2",
+    Rock = "#ba6",
+    Ghost = "#66b",
+    Dragon = "#76e",
+    Dark = "#754",
+    Steel = "#aab",
+    Fairy = "#e9e",
+}
+
+export type { PokemonTypes };
