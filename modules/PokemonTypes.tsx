4e656ad630909e73d201ce09a92e80d59c6d479a
David Lozano <hello@davidlozano.me>
2021-11-01 10:13:17 +0800

    Add conversions of Pokemon Types to Text

--- modules/PokemonTypes.tsx
+++ modules/PokemonTypes.tsx
@@ -66,5 +66,48 @@ function TextToPokemonType(value: string): PokemonTypes {
     }
 }
 
-export { TextToPokemonType };
-export type { PokemonTypes };
+function PokemonTypeToText(type: PokemonTypes): string {
+    switch (type) {
+        case PokemonTypes.Normal:
+            return "normal";
+        case PokemonTypes.Fire:
+            return "fire";
+        case PokemonTypes.Water:
+            return "water";
+        case PokemonTypes.Electric:
+            return "electric";
+        case PokemonTypes.Grass:
+            return "grass";
+        case PokemonTypes.Ice:
+            return "ice";
+        case PokemonTypes.Fighting:
+            return "fighting";
+        case PokemonTypes.Poison:
+            return "poison";
+        case PokemonTypes.Ground:
+            return "ground";
+        case PokemonTypes.Flying:
+            return "flying";
+        case PokemonTypes.Psychic:
+            return "psychic";
+        case PokemonTypes.Bug:
+            return "bug";
+        case PokemonTypes.Rock:
+            return "rock";
+        case PokemonTypes.Ghost:
+            return "ghost";
+        case PokemonTypes.Dragon:
+            return "dragon";
+        case PokemonTypes.Dark:
+            return "dark";
+        case PokemonTypes.Steel:
+            return "steel";
+        case PokemonTypes.Fairy:
+            return "fairy";
+        default:
+            throw new Error("Not a Pokemon Type");
+    }
+}
+
+export { TextToPokemonType, PokemonTypeToText };
+export { PokemonTypes };
