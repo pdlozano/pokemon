02c98ac326b3c5ae0cfeb845d0c82e2e3d498f91
David Lozano <hello@davidlozano.me>
2021-11-01 10:46:33 +0800

    Add Move component
    Still need to figure out how to add Moves

--- components/Move.tsx
+++ components/Move.tsx
@@ -1,21 +1,68 @@
-import type { PokemonTypes } from "../modules/PokemonTypes";
+import type { PokemonMove, Move, MoveClient } from "pokenode-ts";
+import { PokemonTypes, TextToPokemonType } from "../modules/PokemonTypes";
+import { useState, useEffect } from "react";
 
 type MoveData = {
+    data: PokemonMove;
+    api: MoveClient;
+};
+
+type Data = {
     name: string;
-    accuracy: number;
     pp: number;
-    damage: number;
+    accuracy: number;
+    damage_class: string;
     type: PokemonTypes;
 };
 
 function Move(props: MoveData): JSX.Element {
+    const [move, setMove] = useState<Move>();
+    const [data, setData] = useState<Data>();
+
+    useEffect(() => {
+        props.api
+            .getMoveByName(props.data.move.name)
+            .then((res) => setMove(res))
+            .catch((err) => console.error(err));
+    }, []);
+
+    useEffect(() => {
+        if (move === undefined) {
+            return;
+        }
+
+        const name = move.names.filter(
+            (item) => item.language.name === "en"
+        )[0];
+        const pp = move.pp;
+        const accuracy = move.accuracy;
+        const damage_class = move.damage_class.name;
+        const type = TextToPokemonType(move.type.name);
+
+        setData({
+            name: name.name,
+            pp,
+            accuracy,
+            damage_class,
+            type,
+        });
+    }, [move]);
+
+    if (data === undefined) {
+        return <></>;
+    }
+
     return (
-        <div>
-            <p>{props.name}</p>
-            <p>{props.accuracy}</p>
-            <p>{props.pp}</p>
-            <p>{props.damage}</p>
-            <p>{props.type}</p>
+        <div
+            className="btn-move"
+            style={{
+                background: data.type,
+            }}
+        >
+            <p>{data.name}</p>
+            <p>{data.pp}</p>
+            <p>{data.accuracy}</p>
+            <p>{data.damage_class}</p>
         </div>
     );
 }
--- components/MoveSet.tsx
+++ components/MoveSet.tsx
@@ -1,22 +1,41 @@
 import type { PokemonMove } from "pokenode-ts";
-import { useState } from "react";
+import { MoveClient } from "pokenode-ts";
+import Move from "./Move";
+
+const api = new MoveClient();
 
 type MoveSetData = {
-    available: Readonly<Array<PokemonMove>>;
+    moves: Readonly<Array<PokemonMove>>;
+    setMoves: () => null;
 };
 
 function MoveSet(props: MoveSetData): JSX.Element {
-    const [moves, setMoves] = useState<Array<PokemonMove>>([]);
+    if (props.moves.length > 4) {
+        throw new Error("Moves cannot be more than 4");
+    }
+    const moves = props.moves.map((move) => {
+        return <Move data={move} api={api} key={move.move.name} />;
+    });
+    const missing = new Array(4 - moves.length).fill(0).map((_, i) => {
+        return (
+            <button
+                key={i}
+                onClick={(e) => {
+                    e.preventDefault();
+                    props.setMoves();
+                }}
+                className="btn-move"
+            >
+                {i}
+            </button>
+        );
+    });
+    const moveSet = moves.concat(missing);
 
     return (
         <>
             <h2>Moves</h2>
-            <div className="w-full flex flex-wrap">
-                <button className="btn-move">1</button>
-                <button className="btn-move">2</button>
-                <button className="btn-move">3</button>
-                <button className="btn-move">4</button>
-            </div>
+            <div className="w-full flex flex-wrap">{moveSet}</div>
         </>
     );
 }
