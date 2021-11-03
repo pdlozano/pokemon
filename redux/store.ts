916a714a670acdc80275de14ee22f5e45d796993
David Lozano <hello@davidlozano.me>
2021-11-03 16:09:16 +0800

    Fix wrong initialState

--- redux/store.ts
+++ redux/store.ts
@@ -3,7 +3,14 @@ import rootReducer from "./reducers";
 
 const initialState = {
     pokemonData: {
-        pokemon: [],
+        pokemon: {
+            1: null,
+            2: null,
+            3: null,
+            4: null,
+            5: null,
+            6: null,
+        },
     },
 };
 
