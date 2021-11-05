26ebab561d91c5a64a57c8e9a4516473d8a5c8d6
David Lozano <hello@davidlozano.me>
2021-11-05 14:04:22 +0800

    Remove logging

--- components/Coverage.tsx
+++ components/Coverage.tsx
@@ -8,7 +8,6 @@ function Coverage(): JSX.Element {
     const items = Object.values(data)
         .map((item) => {
             return Object.values(item.moves).reduce((prev, next) => {
-                console.log(next);
                 if (next.damage_class.name === "status") {
                     return prev.concat([]);
                 }
