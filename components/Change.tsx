56c76e897c92e2c060fec01d7fe7994be15f2f7b
David Lozano <hello@davidlozano.me>
2021-11-08 13:31:50 +0800

    Add dropdown search to Change
    Don't worry - it's native dropdown search

--- components/Change.tsx
+++ components/Change.tsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 
 type ChangeData = {
     func: (text: string) => void;
-    children?: JSX.Element | Array<JSX.Element>;
+    children?: JSX.Element | Array<JSX.Element> | string[];
+    available?: Array<string>;
 };
 
 function Change(props: ChangeData) {
     const [text, setText] = useState<string>("");
+    const random = Math.floor(Math.random() * 1000);
 
     return (
         <div>
@@ -17,7 +19,14 @@ function Change(props: ChangeData) {
                     setText(event.target.value);
                 }}
                 value={text}
+                className="border-2 border-black"
+                list={"items" + random}
             />
+            <datalist id={"items" + random}>
+                {props.available?.map((item) => (
+                    <option key={item}>{item}</option>
+                ))}
+            </datalist>
             <button
                 onClick={(event) => {
                     event.preventDefault();
