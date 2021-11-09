641fc310ac9f36a7bb0ff35af6c496896878ebb0
David Lozano <hello@davidlozano.me>
2021-11-09 16:13:18 +0800

    Improve look of Change

--- components/Change.tsx
+++ components/Change.tsx
@@ -10,26 +10,28 @@ function Change(props: ChangeData) {
     const [text, setText] = useState<string>("");
 
     return (
-        <div>
-            <input
-                type="text"
-                onChange={(event) => {
-                    event.preventDefault();
-                    setText(event.target.value);
-                }}
-                value={text}
-                list={props.list}
-                className="w-full border-2 border-black bg-white font-bold px-2 py-1 uppercase mb-2"
-            />
-            <button
-                onClick={(event) => {
-                    event.preventDefault();
-                    props.func(text);
-                }}
-                className="w-full border-2 border-red-800 bg-white font-bold px-2 py-1 uppercase hover:bg-red-200"
-            >
-                {props.children}
-            </button>
+        <div className="border-2 border-gray-500 px-4 py-2 flex items-center">
+            <div className="w-full">
+                <input
+                    type="text"
+                    onChange={(event) => {
+                        event.preventDefault();
+                        setText(event.target.value);
+                    }}
+                    value={text}
+                    list={props.list}
+                    className="w-full border-2 border-black bg-white font-bold px-2 py-1 uppercase mb-2"
+                />
+                <button
+                    onClick={(event) => {
+                        event.preventDefault();
+                        props.func(text);
+                    }}
+                    className="w-full border-2 border-red-800 bg-white font-bold px-2 py-1 uppercase hover:bg-red-200"
+                >
+                    {props.children}
+                </button>
+            </div>
         </div>
     );
 }
